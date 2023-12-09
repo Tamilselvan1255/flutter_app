c57277bf0a894b86b479dc3fcd85a594313b1159
Tamilselvan1255 <tamilselvan0677@gmail.com>
2023-12-09 16:08:02 +0530

    ssl

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ require('dotenv').config();  // Make sure this line is at the top
 const express = require('express');
 const mongoose = require('mongoose');
 const authController = require('./controllers/authController');
+const fs = require('fs');
+
+const ca = [fs.readFileSync('path/to/ca.crt')];
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,6 +17,9 @@ app.use(express.json());
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  ssl: true,
+  sslCA: ca
 });
 
 const db = mongoose.connection;
