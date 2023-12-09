bde7c4fd7dce4b17914f2616ef3b6156ad3b4913
Tamilselvan1255 <tamilselvan0677@gmail.com>
2023-12-09 19:14:58 +0530

    bcrypt uninstalled

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-// index.js
 require('dotenv').config();  // Make sure this line is at the top
 
 const express = require('express');
 const mongoose = require('mongoose');
 const authController = require('./controllers/authController');
-const fs = require('fs');
-
-const ca = [fs.readFileSync('path/to/ca.crt')];
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -17,9 +13,7 @@ app.use(express.json());
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  ssl: true,
-  sslCA: ca
+  useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
