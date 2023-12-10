2edb080bd42b89a6e90e8815133c60857c9bf763
Tamilselvan1255 <tamilselvan0677@gmail.com>
2023-12-10 14:25:41 +0530

    updated

--- index.js
+++ index.js
@@ -16,19 +16,37 @@ app.use(cors(corsOption));
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  ssl: true,
+  tlsInsecure: true
 });
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on('error', (error) => {
+  console.error('MongoDB connection error:', error);
+
+  // Check if the error is a ServerSelectionError
+  if (error.name === 'MongoServerSelectionError') {
+    console.error('MongoDB ServerSelectionError:', error.message);
+    // Additional troubleshooting steps can be added here
+  }
+
+  // Exit the application or handle the error based on your needs
+  process.exit(1);
+});
+
 db.once('open', () => {
   console.log('Connected to MongoDB');
+  // Start the server after successful MongoDB connection
+  startServer();
 });
 
 // Routes
 app.use('/auth', authController);
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// Start the server function
+function startServer() {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
